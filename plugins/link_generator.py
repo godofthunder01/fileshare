b35e48e07dead5a61b990a4c7571e6f93067c9f1
technical_aks <105633032+technicalaks@users.noreply.github.com>
2023-04-12 14:48:06 +0530

    Remove admin

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -40,7 +40,7 @@ async def batch(client: Client, message: Message):
     await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
+@Bot.on_message(filters.private & filters.command('genlink'))
 async def link_generator(client: Client, message: Message):
     while True:
         try:
