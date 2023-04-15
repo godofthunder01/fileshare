45c08d5983d76c3e5d541d0f1ac2533167d2f321
Rishikesh Sharma(Rk_botz) <118276768+Rishi09090909@users.noreply.github.com>
2023-04-15 12:52:41 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -42,19 +42,20 @@ async def batch(client: Client, message: Message):
 
 @Bot.on_message(filters.private & filters.command('genlink'))
 async def link_generator(client: Client, message: Message):
-    while True:
-        try:
-            channel_message = await client.ask(text = "Forward Message from the DB Channel (with Quotes)..\nor Send the DB Channel Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
-        except:
-            return
-        msg_id = await get_message_id(client, channel_message)
-        if msg_id:
-            break
-        else:
-            await channel_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote = True)
-            continue
+    try:
+        chat_id = message.chat.id
+        post_message = await client.send_message(chat_id, "Please Wait...!")
+        msg_id = post_message.message_id
+    except Exception as e:
+        print(e)
+        await message.reply_text("Something went Wrong..!")
+        return
 
-    base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
+    converted_id = msg_id * abs(chat_id)
+    string = f"get-{converted_id}"
+    base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
+
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+
+    await post_message.edit_text(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview=True)
